93fe62e8bbea5b4eaa6786071753bcb120798de6
Manikandan S <mani.s.5051@gmail.com>
2023-11-24 15:13:49 +0530

    update

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 resource "google_storage_bucket" "bucket" {
-  name     = "test-bucket-manikandan-project-1234"
+  name     = "test-bucket-manikandan1-project-1234"
   location = "europe-west2"
 }
 
 resource "google_storage_bucket" "gcs_bucket" {
-  name     = "test-bucket-manikandan-project-5678"
+  name     = "test-bucket-manikandan1-project-5678"
   location = "europe-west2"
 }
